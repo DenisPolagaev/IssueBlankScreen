69fea4082d82d11bc51ee6e516cff4651efbb782
Denis Polagaev <polagaev.denis@inbox.ru>
2022-07-11 13:17:09 +0700

    Initial commit

---
+++ IssueBlankScreen/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace IssueBlankScreen;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ IssueBlankScreen/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IssueBlankScreen;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
